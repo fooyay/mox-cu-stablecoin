24156ff862699cacd40e71a952fa020ae7932a86
Sean Coates <sean.coates+fooyay@gmail.com>
2026-03-18 15:59:13 -0400

    test reverts if token not allowed

--- tests/unit/test_dsc_engine.py
+++ tests/unit/test_dsc_engine.py
@@ -17,5 +17,12 @@ def test_reverts_if_token_lengths_are_different(
 def test_reverts_if_collateral_zero(some_user, weth, dsc_engine):
     with boa.env.prank(some_user):
         weth.approve(dsc_engine, COLLATERAL_AMOUNT)
-        with boa.reverts():
+        with boa.reverts("DSCEngine: Needs more than zero collateral"):
             dsc_engine.deposit_collateral(weth, 0)
+
+
+def test_reverts_if_token_not_allowed(some_user, dsc_engine):
+    bad_token = "0x0000000000000000000000000000000000000000"
+    with boa.env.prank(some_user):
+        with boa.reverts("DSCEngine: Token not supported"):
+            dsc_engine.deposit_collateral(bad_token, 1)
