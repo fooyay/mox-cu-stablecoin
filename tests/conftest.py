81c47d93dd78f8a28a207aecbd4e69f3e9c0f80a
Sean Coates <sean.coates+fooyay@gmail.com>
2026-03-18 15:12:36 -0400

    test revert if collateral zero

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
+import boa
+from eth_account import Account
+from eth_utils import to_wei
 from moccasin.config import get_active_network
 from script.deploy_dsc_engine import deploy_dsc_engine
+
+INITIAL_BALANCE: int = to_wei(10, "ether")
+COLLATERAL_AMOUNT: int = to_wei(10, "ether")
 
 
 # session scoped fixtures
@@ -36,5 +42,17 @@ def dsc(active_network):
 
 
 @pytest.fixture
-def dsc_engine(dsc, wbtc, weth, btc_usd_price_feed, eth_usd_price_feed):
-    return deploy_dsc_engine(dsc, wbtc, weth, btc_usd_price_feed, eth_usd_price_feed)
+# def dsc_engine(dsc, wbtc, weth, btc_usd_price_feed, eth_usd_price_feed): # Patrick said we needed these but I don't think so.
+def dsc_engine(dsc):
+    return deploy_dsc_engine(dsc)
+
+
+@pytest.fixture
+def some_user(wbtc, weth):
+    entropy = 13
+    account = Account.create(entropy)
+    boa.env.set_balance(account.address, INITIAL_BALANCE)
+    with boa.env.prank(account.address):
+        wbtc.mock_mint()
+        weth.mock_mint()
+    return account.address
--- tests/unit/test_dsc_engine.py
+++ tests/unit/test_dsc_engine.py
@@ -1,6 +1,8 @@
 import pytest
+import boa
 from eth.codecs.abi.exceptions import EncodeError
 from src import dsc_engine
+from tests.conftest import COLLATERAL_AMOUNT
 
 
 def test_reverts_if_token_lengths_are_different(
@@ -10,3 +12,10 @@ def test_reverts_if_token_lengths_are_different(
         dsc_engine.deploy(
             [wbtc, weth, weth], [btc_usd_price_feed, eth_usd_price_feed], dsc
         )
+
+
+def test_reverts_if_collateral_zero(some_user, weth, dsc_engine):
+    with boa.env.prank(some_user):
+        weth.approve(dsc_engine, COLLATERAL_AMOUNT)
+        with boa.reverts():
+            dsc_engine.deposit_collateral(weth, 0)
